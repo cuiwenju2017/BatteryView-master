c8dea57a85d486f09b98d165458d6bcdf0a793c2
崔文举 <1755140651@qq.com>
2019-09-24 15:41:06 +0800

    Initial commit

---
+++ app/src/main/java/com/single/battery/MainActivity.java
@@ -0,0 +1,43 @@
+package com.single.battery;
+
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.ViewTreeObserver;
+
+import com.library.battery.BatteryStatus;
+import com.library.battery.BatteryView;
+
+import java.util.Timer;
+
+public class MainActivity extends AppCompatActivity {
+
+    BatteryView batteryView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        batteryView = findViewById(R.id.batteryView);
+        batteryView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                batteryView.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+                IntentFilter filter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+                registerReceiver(new BatteryReceiver(), filter);
+            }
+        });
+    }
+
+    public void setBattery(BatteryStatus status) {
+        Log.d("TAG", "status:" + status.status + ",level:" + status.level);
+        batteryView.setChanges(status.status, status.level);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
